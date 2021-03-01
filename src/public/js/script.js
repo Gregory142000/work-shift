8a85dc2e845b8d6fd07ac279c04d76c1ac675b9a
Gregory Vicent <gregory142000@gmail.com>
2021-03-01 19:39:18 -0400

    test

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,15 +1,13 @@
 import CRUDEmployee from "./adminPanel/CRUDEmployee.js";
 import selectWork from "./selectWorkZone/selectWork.js";
 import headerController from "./headerController.js";
-import noAccessShift from "./selectWorkZone/noAccessShift.js"
+import noAccessShift from "./selectWorkZone/noAccessShift.js";
 
 let pathname = window.location.pathname;
 
 document.addEventListener("DOMContentLoaded", () => {
-  if(pathname === '/') {
-    noAccessShift();
-    selectWork();
-    headerController();
-  } 
-  if(pathname === '/admin.php') CRUDEmployee();
+  noAccessShift();
+  selectWork();
+  headerController();
+  CRUDEmployee();
 });
