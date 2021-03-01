9ea4ee8836cecde5bd05bad331cb6f14fa5f8ed2
Gregory Vicent <gregory142000@gmail.com>
2021-03-01 19:44:22 -0400

    test

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,13 +1,15 @@
 import CRUDEmployee from "./adminPanel/CRUDEmployee.js";
 import selectWork from "./selectWorkZone/selectWork.js";
 import headerController from "./headerController.js";
-import noAccessShift from "./selectWorkZone/noAccessShift.js";
+import noAccessShift from "./selectWorkZone/noAccessShift.js"
 
 let pathname = window.location.pathname;
 
 document.addEventListener("DOMContentLoaded", () => {
-  noAccessShift();
-  selectWork();
-  headerController();
-  CRUDEmployee();
+  if(pathname === '/') {
+    noAccessShift();
+    selectWork();
+    headerController();
+  } 
+  if(pathname === '/admin.php') CRUDEmployee();
 });
--- src/public/js/startCalendar.js
+++ src/public/js/startCalendar.js
@@ -1,91 +1,14 @@
-// import startCalendar from "./fullCalendar/calendar.js";
-// import selectShifts from "./fullCalendar/selectShifts.js";
-
-// fetch("../helper/WorkShiftAPI.php")
-//   .then((res) => res.json())
-//   .then((res) => {
-//     let weekShifts = selectShifts(res);
-
-//     return weekShifts;
-//   })
-//   .then((worksOfWeek) => startCalendar(worksOfWeek))
-//   .catch((error) => {
-//     console.log(error); //Crea una solución para el catch
-//   });
+import startCalendar from "./fullCalendar/calendar.js";
+import selectShifts from "./fullCalendar/selectShifts.js";
 
 fetch("../helper/WorkShiftAPI.php")
   .then((res) => res.json())
   .then((res) => {
-    let weekShifts = [],
-      count = 1;
-
-    res.forEach((el) => {
-      let shiftObject = {},
-        weekDays = [],
-        employeeId = null;
-
-      if (el.sunday) {
-        weekDays.push(0);
-        employeeId = el.sunday;
-      }
-      if (el.monday) {
-        weekDays.push(1);
-        employeeId = el.monday;
-      }
-      if (el.tuesday) {
-        weekDays.push(2);
-        employeeId = el.tuesday;
-      }
-      if (el.wednesday) {
-        weekDays.push(3);
-        employeeId = el.wednesday;
-      }
-      if (el.thursday) {
-        weekDays.push(4);
-        employeeId = el.thursday;
-      }
-      if (el.friday) {
-        weekDays.push(5);
-        employeeId = el.friday;
-      }
-      if (el.saturday) {
-        weekDays.push(6);
-        employeeId = el.saturday;
-      }
-
-      shiftObject = {
-        id: count++,
-        title: `${el.shift_number} | Employee: ${employeeId}`,
-        daysOfWeek: weekDays,
-      };
-
-      weekShifts.push(shiftObject);
-    });
-
-    return weekShifts;
+    let weekShifts = selectShifts(res);
 
     return weekShifts;
   })
-  .then((worksOfWeek) => {
-    document.addEventListener("DOMContentLoaded", function () {
-      var calendarEl = document.getElementById("calendar");
-      var calendar = new FullCalendar.Calendar(calendarEl, {
-        events: worksOfWeek,
-        initialView: "dayGridMonth",
-        headerToolbar: {
-          end: false,
-        },
-        showNonCurrentDates: false,
-        dayMaxEventRows: true,
-        views: {
-          timeGrid: {
-            dayMaxEventRows: 10,
-          },
-        },
-      });
-      calendar.render();
-    });
-  })
+  .then((worksOfWeek) => startCalendar(worksOfWeek))
   .catch((error) => {
     console.log(error); //Crea una solución para el catch
   });
