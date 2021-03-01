61ebea153d02a1cc3dc6b6c67d228875ec079d52
Gregory Vicent <gregory142000@gmail.com>
2021-03-01 19:46:37 -0400

    test

--- src/lib/model/DbConfig.php
+++ src/lib/model/DbConfig.php
@@ -1,12 +1,12 @@
 <?php
 
-// const DB_NAME = "dcje0s5vfa6tkt";
-// const DB_HOST = "ec2-54-164-238-108.compute-1.amazonaws.com";
-// const DB_USER = "avvqlbkfxigiop";
-// const DB_PASSWORD = "e7b80d13ebd4c939080dacc8db872c13a6cfa77d3af8a9360f6af392a5729a8f";
+const DB_NAME = "dcje0s5vfa6tkt";
+const DB_HOST = "ec2-54-164-238-108.compute-1.amazonaws.com";
+const DB_USER = "avvqlbkfxigiop";
+const DB_PASSWORD = "e7b80d13ebd4c939080dacc8db872c13a6cfa77d3af8a9360f6af392a5729a8f";
 
 
-const DB_NAME = "work_shift_app";
-const DB_HOST = "localhost";
-const DB_USER = "postgres";
-const DB_PASSWORD = "root";
+// const DB_NAME = "work_shift_app";
+// const DB_HOST = "localhost";
+// const DB_USER = "postgres";
+// const DB_PASSWORD = "root";
